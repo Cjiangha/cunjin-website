d1d9f3d4756472a8cb25e05143d46279f462e5de
Cjiangha <445620536@qq.com>
2022-05-16 22:10:17 +0800

    feat:选项卡

--- school website/js/index.js
+++ school website/js/index.js
@@ -34,16 +34,44 @@
         time: 3 //(可选参数)
     })
 
+
+    let hasMoreconfirst = $('.part3 .hasMoreTab .bd>.con .xyxx_list:first-child')
+    let hasMoreconlast = $('.part3 .hasMoreTab .bd>.con .xyxx_list:last-child')
     //校园快讯
     let hdul = $('.part3 .hasMoreTab .hd').find('ul li')
     hdul.on('click',function(){
        $(this).addClass('on').siblings().removeClass('on')
+       let id =    $(this).data('id')
+       if($(this).attr('class') === 'on'){
+           console.log(id)
+            if(id === 1){
+                hasMoreconfirst.css({'display':'block'})
+                hasMoreconlast.css({'display':'none'})
+            }else if(id ===2){
+                hasMoreconfirst.css({'display':'none'})
+                hasMoreconlast.css({'display':'block'})
+            }
+       }
     })
+    
+    let hastzggconfirst = $('.part3 .tzgg .bd>.con .xyxx_list:first-child')
+    let hastzggconlast = $('.part3 .tzgg .bd>.con .xyxx_list:last-child')
     let tzgg = $('.part3 .tzgg .hd').find('ul li')
     tzgg.on('click',function(){
        $(this).addClass('on').siblings().removeClass('on')
+       let id =  $(this).data('id')
+       if($(this).attr('class') === 'on'){
+           console.log(id)
+            if(id === 1){
+                hastzggconfirst.css({'display':'block'})
+                hastzggconlast.css({'display':'none'})
+            }else if(id ===2){
+                hastzggconfirst.css({'display':'none'})
+                hastzggconlast.css({'display':'block'})
+            }
+       }
     })
- 
+    
 
     // swiper
      new Swiper('.swiper-container', {
---
+++ school website/js/xuanrang.js
@@ -0,0 +1,30 @@
+// 校园咨询
+let xiaoyuanzixunHTML = `  <ul class="xyxx_list clearfix" style="display:none;">
+<li class="clearfix"><a href="javascript:void(0);" class="clearfix">
+        <div class="date"><i class="day">21</i><i class="year">2022-04</i></div>
+        <div class="infor"><b>我校教师受邀参加“海洋文化遗产保护与利用”学术研讨会并作主题报告<img src="./img/new2.png" /></b>
+        </div>
+    </a></li>
+<li class="clearfix"><a href="javascript:void(0);" class="clearfix">
+        <div class="date"><i class="day">21</i><i class="year">2022-04</i></div>
+        <div class="infor"><b>我校教师受邀参加“海洋文化遗产保护与利用”学术研讨会并作主题报告</b></div>
+    </a></li>
+<li class="clearfix"><a href="javascript:void(0);" class="clearfix">
+        <div class="date"><i class="day">21</i><i class="year">2022-04</i></div>
+        <div class="infor"><b>我校教师受邀参加“海洋文化遗产保护与利用”学术研讨会并作主题报告</b></div>
+    </a></li>
+<li class="clearfix"><a href="javascript:void(0);" class="clearfix">
+        <div class="date"><i class="day">21</i><i class="year">2022-04</i></div>
+        <div class="infor"><b>我校教师受邀参加“海洋文化遗产保护与利用”学术研讨会并作主题报告</b></div>
+    </a></li>
+<li class="clearfix"><a href="javascript:void(0);" class="clearfix">
+        <div class="date"><i class="day">21</i><i class="year">2022-04</i></div>
+        <div class="infor"><b>我校教师受邀参加“海洋文化遗产保护与利用”学术研讨会并作主题报告</b></div>
+    </a></li>
+</ul>`
+
+let con = $('.part3 .bd>.con')
+con.append(xiaoyuanzixunHTML)
+
+
+
